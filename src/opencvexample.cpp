a8d645a135c4aa8c6b118ee05ddd23c8dc62826f
Ollie Hayman <discorev@olliespage.net>
2015-03-05 12:25:13 +0000

    Took contour of the depth data to ensure that noise inside of the object is removed during depth masking.

--- src/opencvexample.cpp
+++ src/opencvexample.cpp
@@ -24,7 +24,7 @@ int main(int argc, char * argv[])
 
 	// Create the RGB and Depth Windows
 	cv::namedWindow("RGB", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Depth", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Object", cv::WINDOW_AUTOSIZE);
 
 	// The key value represents a key pressed on the keyboard,
 	// where 27 is the ESCAPE key
@@ -39,9 +39,6 @@ int main(int argc, char * argv[])
 	// The status can by bitwise AND to determine if the RGB or
 	// Depth image has been updated using the State enum.
 	uint8_t status = 255;
-    
-    bool oneSet = false;
-    bool bothSet = false;
 
 	while (key != 27 && status != 0)
 	{
@@ -58,9 +55,8 @@ int main(int argc, char * argv[])
         // create a grayscale from the RGB
         cv::Mat src_gray;
         cv::cvtColor( currentRGB, src_gray, cv::COLOR_RGB2GRAY );
-        //cv::adaptiveThreshold(src_gray, src_gray, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY_INV, 3, 0);
-        //std::cout << cv::mean(src_gray) << std::endl;
         cv::threshold( src_gray, src_gray, 190, 255,cv::THRESH_BINARY);
+        //cv::blur( src_gray, src_gray, cv::Size(10,10) );
         
 		// Determine if Depth is updated, and grabs the image
 		// if it has been updated
@@ -82,26 +78,82 @@ int main(int argc, char * argv[])
         if(cv::mean(image_roi)[0] == 255)
             thresholded = cv::Mat::zeros(thresholded.rows, thresholded.cols, CV_8UC1); // create a zero matrix
         
+        // ------   MASKING THE RGB IMAGE   -------
+        
         // create a translation matrix to translate the depth image to bring it more inline with the RGB
         cv::Mat trans_mat = (cv::Mat_<double>(2,3) << 1, 0, -38, 0, 1, 25);
         // apply the transform to the depth image
         cv::warpAffine(thresholded,thresholded,trans_mat,thresholded.size());
         
+        // ------ END MASKING THE RGB IMAGE -------
+        
+        // clean up the thresholded RGB image and then
         cv::bitwise_and(thresholded, src_gray, src_gray);
-        //cv::bitwise_or(src_gray, thresholded, src_gray);
+        cv::bitwise_or(thresholded, src_gray, thresholded);
         
-        // convert the depth image back into RGB channels from grayscale
-        cvtColor(thresholded,thresholded, cv::COLOR_GRAY2BGR);
+        // ------ DETECT CONTOURS AND OBJECT IN IMAGE ------
         
-        // create a new matrix to hold the masked image and apply a bit mask based on the depth
-        cv::Mat masked;
-        cv::bitwise_and(currentRGB, thresholded, masked);
+        std::vector<std::vector<cv::Point> > contours;
+        std::vector<cv::Vec4i> hierarchy;
+        
+        cv::findContours( thresholded, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE );
+        
+        double largest_area=0;
+        int largest_contour_index=0;
+        
+        for( int i = 0; i < contours.size(); i++ )
+        {
+            double a = cv::contourArea( contours[i],false);
+            if(a > largest_area)
+            {
+                largest_area = a; largest_contour_index = i;
+            }
+        }
+        
+        /// Approximate contours to polygons + get bounding rects and circles
+        std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+        cv::Rect boundRect;
+        cv::Point2f center;
+        float radius;
+        
+        for( int i = 0; i < contours.size(); i++ )
+        {
+            if(i == largest_contour_index)
+            {
+                cv::approxPolyDP( cv::Mat(contours[largest_contour_index]), contours_poly[largest_contour_index], 3, true );
+                boundRect = cv::boundingRect( cv::Mat(contours_poly[largest_contour_index]) );
+                cv::minEnclosingCircle( (cv::Mat)contours_poly[largest_contour_index], center, radius );
+            }
+        }
+        
+        /// Draw polygonal contour + bonding rects + circles
+        cv::Mat drawing = cv::Mat::zeros( src_gray.size(), CV_8UC3 );
+        cv::Mat masked = cv::Mat::zeros( src_gray.size(), CV_8UC3 );
+        for( int i = 0; i< contours.size(); i++ )
+        {
+            if(i == largest_contour_index)
+            {
+                cv::Scalar color = cv::Scalar( 255, 0, 0 );
+                // if the bound rect bottom right is 590 or higher, the object is  still entering
+                // the image
+                if(boundRect.br().x <= 590)
+                    cv::drawContours( masked, contours_poly, i, cv::Scalar(255,255,255), cv::FILLED );
+                cv::drawContours( drawing, contours_poly, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+                cv::rectangle( drawing, boundRect.tl(), boundRect.br(), color, 2, 8, 0 );
+                cv::circle( drawing, center, (int)radius, color, 2, 8, 0 );
+            }
+        }
+        
+        // ------ END DETECTING CONTOURS ------
+        
+        
+        // use the contour region as a mask
+        cv::bitwise_and(currentRGB, masked, masked);
         
 		// Show the images in the windows
         cv::imshow("RGB", currentRGB);
-        cv::imshow("Grayscale", src_gray);
+        cv::imshow("Object", drawing);
         cv::imshow("Masked", masked);
-        cv::imshow("Depth", thresholded);
         
 		// Check for keyboard input
 		key = cv::waitKey(10);
